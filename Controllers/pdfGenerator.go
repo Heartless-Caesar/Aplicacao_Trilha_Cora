74a691a912f8718fc044cf412748c0d8157d9aeb
cesa <joaovictorcsatu@gmail.com>
2022-12-18 00:12:36 -0300

    update

--- Controllers/pdfGenerator.go
+++ Controllers/pdfGenerator.go
@@ -4,9 +4,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/johnfercher/maroto/pkg/consts"
 	"github.com/johnfercher/maroto/pkg/pdf"
+
+	"net/http"
 )
 
-func GeneratePdf(c *fiber.Ctx) error {
+func GeneratePdf(c *fiber.Ctx, w http.ResponseWriter) error {
+
 	//Criando o novo PDF
 	m := pdf.NewMaroto(consts.Landscape, consts.A4)
 
@@ -23,7 +26,21 @@ func GeneratePdf(c *fiber.Ctx) error {
 		})
 	}
 
+	w.Header().Set("Content-Disposition", "attachment; filename=certificate.pdf")
+
+	w.Header().Set("Content-Type", c.GetRespHeader("Content-Type"))
+
 	return c.JSON(fiber.Map{
 		"message": "PDF created",
+	})
+}
+
+func buildHeader(m pdf.Maroto) {
+	m.RegisterHeader(func() {
+		m.Row(50, func() {
+			m.Col(12, func() {
+
+			})
+		})
 	})
 }
