41e5589736d6d2d6ab56b40b90fc2bebf7ed9b0f
cesa <joaovictorcsatu@gmail.com>
2022-12-20 18:30:39 -0300

    Added router to app component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,13 +3,30 @@ import "./App.css";
 import { useEffect } from "react";
 import Login from "./components/Login";
 import HomePage from "./components/Homepage";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 /* 
   TODO Add main page component where the context is provided to all components
   ! Add context so that user persistence is possible
 */
 
 export const UserContext = createContext({});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+    children: [
+      {
+        path: "/Home",
+        element: <App />,
+      },
+      {
+        path: "/Register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
 function App() {
   const [user, setUser] = useState("");
@@ -25,7 +42,7 @@ function App() {
   return (
     <div className="App">
       <UserContext.Provider value={{ logged, setLogged }}>
-        {logged == true ? <HomePage /> : <Login />}
+        <RouterProvider router={router} />
       </UserContext.Provider>
     </div>
   );
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,31 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import { createContext } from "react";
 import React from "react";
 import App from "./App";
 import "./index.css";
-import Login from "./components/Login";
-import Register from "./components/Register";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-    children: [
-      {
-        path: "/Home",
-        element: <App />,
-      },
-      {
-        path: "/Register",
-        element: <Register />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
