6aa0e04fb8b8607128a2543aa42a4761160215c0
cesa <joaovictorcsatu@gmail.com>
2022-12-25 22:51:59 -0300

    Added rate limiter

--- API/main.go
+++ API/main.go
@@ -4,9 +4,11 @@ import (
 	database "app_trilha/Database"
 	routes "app_trilha/Routes"
 	configutils "app_trilha/Utils"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func main() {
@@ -18,6 +20,11 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
+	}))
+
+	app.Use(limiter.New(limiter.Config{
+		Max: 20,
+		Expiration: 30 * time.Second,
 	}))
 
 	routes.Setup(app)
