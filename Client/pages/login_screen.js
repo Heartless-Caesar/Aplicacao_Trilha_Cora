9bcc498ddbe1c3047f674cac9c2a735dfb151aaa
cesa <joaovictorcsatu@gmail.com>
2022-12-27 09:16:53 -0300

    cleared logi nscreen and added background image

--- Client/pages/login_screen.js
+++ Client/pages/login_screen.js
@@ -1,4 +1,10 @@
-import { View, Text, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  ImageBackground,
+} from "react-native";
 import React, { useState } from "react";
 import loginStyle from "../styles/login_style";
 
@@ -7,23 +13,36 @@ const Login_screen = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  return (
-    <View style={loginStyle.container}>
-      <View style={loginStyle.credentials_container}>
-        <Text>Login_screen</Text>
-        <TextInput
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-        />
-        <TextInput
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-      </View>
-    </View>
-  );
+  return <View style={loginStyle.container}></View>;
 };
+
+{
+  /* <ImageBackground
+        source={{ uri: "../assets/background-img.jpg" }}
+        resizeMode="cover"
+        style={loginStyle.image}
+      >
+        <View style={loginStyle.input}>
+          <TextInput
+            style={loginStyle.textInput}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+        </View>
+        <View style={loginStyle.input}>
+          <TextInput
+            style={loginStyle.textInput}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+        </View>
+        <Pressable style={loginStyle.loginBtn}>
+          <Text>Login</Text>
+        </Pressable>
+      </ImageBackground> */
+}
 
 export default Login_screen;
--- Client/styles/login_style.js
+++ Client/styles/login_style.js
@@ -3,10 +3,39 @@ import { StyleSheet } from "react-native";
 const login = StyleSheet.create({
   container: {
     flex: 1,
+    //flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  credentials_container: {
-    flexDirection: "column",
-    justifyContent: "space-between",
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  input: {
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+    backgroundColor: "#FFF3E0",
+  },
+  textInput: {
+    justifyContent: "center",
+    height: 50,
+    flex: 1,
+    padding: 20,
+    marginLeft: 20,
+    paddingLeft: 30,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FFB74D",
   },
 });
 
