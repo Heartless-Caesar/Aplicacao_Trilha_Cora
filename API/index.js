782261912dd15adf948b0b089379aa1d23ad450d
cesa <joaovictorcsatu@gmail.com>
2022-12-30 15:47:12 -0300

    Update

--- API/index.js
+++ API/index.js
@@ -3,14 +3,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { sequelize } = require("./models/index.js");
 const { auth_router } = require("./routes/auth_routes.js");
-
+require("dotenv").config();
 //Auth endpoints
 app.use(auth_router);
 
 const start = async () => {
   try {
-    await sequelize.sync({ force: true });
-    console.log("Connected to DB...");
+    await sequelize.sync();
+    console.log(`Connected to DB in ${process.env.NODE_ENV} environment`);
     app.listen(port, () => {
       console.log(`App listening on port 3000`);
     });
