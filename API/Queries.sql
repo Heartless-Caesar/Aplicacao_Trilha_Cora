37593f11c5d87fd41c5bab595ab6d67ca68d93ac
cesa <joaovictorcsatu@gmail.com>
2022-12-30 14:50:09 -0300

    Added sqlite development db

---
+++ API/Queries.sql
@@ -0,0 +1,8 @@
+-- SQLite
+SELECT name FROM sqlite_master WHERE type="table" ORDER BY NAME;
+
+CREATE TABLE test(
+    test_string VARCHAR(255)
+);
+
+SELECT * FROM test
--- API/config/config.js
+++ API/config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    Storage: "dev.db",
+    storage: "development.db",
     dialect: "sqlite",
   },
   test: {
