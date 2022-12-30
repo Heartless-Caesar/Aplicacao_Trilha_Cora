78ba4d0d9317850bc407f173faff95b5004c14a4
cesa <joaovictorcsatu@gmail.com>
2022-12-30 09:41:11 -0300

    update

--- API/database/config/config.js
+++ API/database/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    password: "887887yua2",
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
