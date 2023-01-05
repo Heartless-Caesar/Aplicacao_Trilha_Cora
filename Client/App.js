efc70c9d7931fd512757293eb5ea4ba57cf1fe56
cesa <joaovictorcsatu@gmail.com>
2023-01-05 09:45:28 -0300

    Fixed client routing

--- Client/App.js
+++ Client/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Login_screen from "./pages/login_screen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import Login_screen from "./pages/login_screen";
 import Register_screen from "./pages/register_page";
 import Homepage from "./pages/home_page";
 
@@ -12,10 +12,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={<Login_screen />} />
-        <Stack.Screen name="Register" component={<Register_screen />} />
-        <Stack.Screen name="Home" component={<Homepage />} />
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={Login_screen} />
+        <Stack.Screen name="Register" component={Register_screen} />
+        <Stack.Screen name="Home" component={Homepage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
