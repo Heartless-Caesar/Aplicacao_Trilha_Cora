bb7e31c822e27d76b43469f590a9e45853d413f3
cesa <joaovictorcsatu@gmail.com>
2023-01-10 13:26:51 -0300

    added sessions to manage user auth

--- API/middleware/deserialize_user.js
+++ API/middleware/deserialize_user.js
@@ -1,9 +1,32 @@
+const { verify_jwt } = require("../utils/jwt_utils");
+
 const deserialize_user = async (req, res, next) => {
-    const { access_token } = req.cookies
+  const { access_token, refresh_token } = req.cookies;
 
-    if (!access_token) {
-        return next()
-    }
+  if (!access_token) {
+    return next();
+  }
 
-    const {} = jwt.verify(access_token, process.env.JWT_SECRET)
-}
+  const { payload, expired } = verify_jwt(access_token);
+
+  // For valid access token
+  if (payload) {
+    req.user = payload;
+
+    return next();
+  }
+
+  //For invalid access token
+  const { payload: refresh } =
+    expired && refresh_token ? verify_jwt(refresh_token) : { payload: null };
+
+  if (!refresh) {
+    return next();
+  }
+
+  //Add session
+
+  return next();
+};
+
+module.exports = { deserialize_user };
