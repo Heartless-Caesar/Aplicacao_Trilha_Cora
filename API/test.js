fcfd822e1792aee4119f52e86e3f606ec5bab12d
cesa <joaovictorcsatu@gmail.com>
2023-02-17 23:52:52 -0300

    update

--- API/test.js
+++ API/test.js
@@ -196,6 +196,7 @@ const test = async (start_code, finish_code, user_id) => {
         );
         break;
       default:
+        console.log("Invalid codes");
         break;
     }
   }
@@ -247,10 +248,114 @@ const test = async (start_code, finish_code, user_id) => {
           {
             SFG: true,
             JRG: true,
+            ITG: true,
           },
           { where: { user_id: user_id } }
         );
         break;
+      case found_codes[0].cdg:
+        await local_validation.update(
+          {
+            SFG: true,
+            JRG: true,
+            ITG: true,
+            CDG: true,
+          },
+          { where: { user_id: user_id } }
+        );
+        break;
+      default:
+        console.log("Invalid codes");
+        break;
+    }
+  }
+
+  // * ITG start_code
+  if (start_code == found_codes[0].itg) {
+    switch (finish_code) {
+      case found_codes[0].jrg:
+        await local_validation.update(
+          {
+            JRG: true,
+            ITG: true,
+          },
+          { where: { user_id: user_id } }
+        );
+        break;
+      case found_codes[0].sfg:
+        await local_validation.update(
+          {
+            JRG: true,
+            ITG: true,
+            SFG: true,
+          },
+          { where: { user_id: user_id } }
+        );
+        break;
+      case found_codes[0].prn:
+        await local_validation.update(
+          {
+            PRN: true,
+            JRG: true,
+            ITG: true,
+            SFG: true,
+          },
+          { where: { user_id: user_id } }
+        );
+        break;
+      case found_codes[0].cog:
+        await local_validation.update(
+          {
+            COG: true,
+            PRN: true,
+            JRG: true,
+            ITG: true,
+            SFG: true,
+          },
+          { where: { user_id: user_id } }
+        );
+        break;
+      case found_codes[0].cbg:
+        await local_validation.update(
+          {
+            CBG: true,
+            COG: true,
+            PRN: true,
+            JRG: true,
+            ITG: true,
+            SFG: true,
+          },
+          { where: { user_id: user_id } }
+        );
+        break;
+      case found_codes[0].cdg:
+        await local_validation.update(
+          {
+            ITG: true,
+            CDG: true,
+          },
+          { where: { user_id: user_id } }
+        );
+        break;
+      default:
+        console.log("Invalid code");
+        break;
+    }
+  }
+
+  // * CDG start_code
+  if (start_code == found_codes[0].cdg) {
+    switch (finish_code) {
+      case found_codes[0].itg:
+        await local_validation.update(
+          {
+            CDG: true,
+            ITG: true,
+          },
+          { where: { user_id: user_id } }
+        );
+        break;
+
       default:
         break;
     }
