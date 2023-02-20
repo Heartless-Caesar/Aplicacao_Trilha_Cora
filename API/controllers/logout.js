966bd727b042ddd47fc8caa91951ab58ba49d28d
cesa <joaovictorcsatu@gmail.com>
2023-02-20 20:35:05 -0300

    added logout controller

---
+++ API/controllers/logout.js
@@ -0,0 +1,26 @@
+const { User } = require("../models");
+const { StatusCodes } = require("http-status-codes");
+
+const handle_refresh_token = async (req, res) => {
+  const cookies = req.cookies;
+
+  if (!cookies?.refresh_token) {
+    return res.status(StatusCodes.NO_CONTENT);
+  }
+
+  const refresh_token = cookies.refresh_token;
+
+  const found_user = await User.findOne({
+    where: { refresh_token: refresh_token },
+  });
+
+  if (!found_user) {
+    res.clearCookie("refresh_token", { httpOnly: true });
+    return res.status(StatusCodes.NO_CONTENT);
+  }
+
+  // * Delete refresh_token in DB
+  await User.update({ refresh_token: "" }, { where: { id: found_user.id } });
+};
+
+module.exports = { handle_refresh_token };
