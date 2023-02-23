0ef512f7ccad27ecf393fe1c7b95370c8e8818a7
cesa <joaovictorcsatu@gmail.com>
2023-02-23 20:46:15 -0300

    update

--- API/pdf-test.js
+++ API/pdf-test.js
@@ -1,47 +1,55 @@
-const pdf = require("pdf-creator-node");
 const fs = require("fs");
 const path = require("path");
+const complete = fs.readFileSync(
+  path.join(`assets/complete_cert.html`),
+  "utf8"
+);
+const partial = fs.readFileSync(path.join(`assets/partial_cert.html`), "utf8");
 const { options } = require("./config/pdf_options");
-const partial = fs.readFileSync(path.join(`assets/partial_cert.html`));
-const complete = fs.readFileSync(path.join(`assets/complete_cert.html`));
+const pdf = require("pdf-creator-node");
+var stream = require("stream");
 
-const pdf_test = async (req, res) => {
-  const { start_local, start_time, finish_local, finish_time, type } =
-    req.query;
-  const document = {};
+const pdf_test = async () => {
+  const start_local = "Cidade de Goias";
+  const start_time = "13:00";
+  const finish_local = "Corumba de Goias";
+  const finish_time = "19:00";
+  const type = "partial";
+  const name = "Cesa";
+  let document = {};
 
   if (type == "partial") {
     document = {
       html: partial,
       data: {
-        name: req.user.name,
-        start_local: start_local,
-        finish_local: finish_local,
-        start_time: start_time,
-        finish_time: finish_time,
-      },
-      path: "../output",
-      type: "Stream",
-    };
-  } else {
-    document = {
-      html: complete,
-      data: {
-        name: req.user.name,
+        name: name,
         start_local: start_local,
         finish_local: finish_local,
         start_time: start_time,
         finish_time: finish_time,
       },
       path: `../output_files/${name}_certificate.pdf`,
-      type: "Stream",
+      type: "stream",
+    };
+  } else {
+    document = {
+      html: complete,
+      data: {
+        name: name,
+        start_local: start_local,
+        finish_local: finish_local,
+        start_time: start_time,
+        finish_time: finish_time,
+      },
+      path: `../output_files/${name}_certificate.pdf`,
+      type: "stream",
     };
   }
 
   // ! START
   // * Download file from memory
   /* 
-  var stream = require('stream');
+  
 //...
 app.get('/download', function(request, response){
   //...
@@ -60,14 +68,16 @@ app.get('/download', function(request, response){
 
   return pdf
     .create(document, options)
-    .then(async (res) => {
+    .then((res) => {
       console.log("Criação de PDF sucedida");
       console.log(res);
     })
-    .catch(async (error) => {
+    .catch((error) => {
       console.log("Algo de errado ocorreu ao tentar gerar o arquivo PDF");
       console.log(`Output error ${error}`);
     });
 };
+
+pdf_test();
 
 module.exports = { pdf_test };
