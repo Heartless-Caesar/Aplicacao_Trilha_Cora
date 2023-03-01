7c6f8b3547770b63c36b96a3be86b865aa0bd77d
Guilherme Gomes <guilherme.gomes6875@gmail.com>
2023-02-28 23:43:53 -0300

    Update modal

--- Client/styles/custom_modal_styles.js
+++ Client/styles/custom_modal_styles.js
@@ -1,16 +1,19 @@
 import { StyleSheet } from "react-native";
 
 const CustomModalStyle = StyleSheet.create({
+
     modal: {
         alignItems: "center",
         justifyContent: "center",
-        height: "90%",
+        height: "70%",
         width: "90%",
         backgroundColor: "#25292e",
         borderTopLeftRadius: 20,
         borderTopRightRadius: 20,
         borderBottomLeftRadius: 20,
-        borderBottomRightRadius: 20
+        borderBottomRightRadius: 20,
+        marginTop: "18%",
+        margin: "5%"
     }
 });
 
