cac778ec1c6c31eccf2ea5e7b0b759ea8de5d144
cesa <joaovictorcsatu@gmail.com>
2023-03-02 00:12:00 -0300

    update

--- API/pdf-test.js
+++ API/pdf-test.js
@@ -46,7 +46,32 @@ const pdf_test = async () => {
   }
 
   pdf
-    .create(document, options)
+    .create(document, {
+      childProcessOptions: {
+        format: "A4",
+        orientation: "landscape",
+        border: "10mm",
+        header: {
+          height: "10mm",
+          contents:
+            '<div style="text-align: center;">Certificado Trilha de Cora Coralina</div>',
+        },
+        footer: {
+          height: "10mm",
+          contents: {
+            first: "Cover page",
+            2: "Second page", // Any page number is working. 1-based index
+            default:
+              '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            last: "Last Page",
+          },
+        },
+        timeout: 10000,
+        env: {
+          OPENSSL_CONF: "/dev/null",
+        },
+      },
+    })
     .then((res) => {
       console.log("Criação de PDF sucedida");
       console.log(res);
