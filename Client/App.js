5204aa534c3d667838d8a64745dff8ce4c090d16
João Victor Cesa Andrade <82420106+Heartless-Caesar@users.noreply.github.com>
2023-08-22 20:53:10 -0300

    Initial commit

--- Client/App.js
+++ Client/App.js
@@ -5,21 +5,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import Login_screen from "./pages/login_screen";
 import Register_screen from "./pages/register_page";
 import Homepage from "./pages/home_page";
+import { UserProvider } from "./utils/userPersistence";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={Login_screen} />
-        <Stack.Screen name="Register" component={Register_screen} />
-        <Stack.Screen name="Home" component={Homepage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserProvider>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Login" component={Login_screen} />
+          <Stack.Screen name="Register" component={Register_screen} />
+          <Stack.Screen name="Home" component={Homepage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
   );
 }
